b83aeccc71c1b9f9794baea1a3e46ad7f85d74e5
newsev <newsev@foxmail.com>
2020-03-01 00:37:42 +0800

    abc

--- src/main/java/com/newsev/git/Happy.java
+++ src/main/java/com/newsev/git/Happy.java
@@ -2,6 +2,9 @@ package com.newsev.git;
 
 public class Happy {
 	public static void main(String[] args) {
+		System.out.println("test conflict 2");
 		System.out.println("test conflict 1");
+		System.out.println("test with hot_fix");
+		System.out.println("abc");
 	}
 }
