9f0aaa4165608d8014c3ceff3e9f6bb2dd4e8730
newsev <newsev@foxmail.com>
2020-03-01 00:42:07 +0800

    test with hot_fix

--- src/main/java/com/newsev/git/Happy.java
+++ src/main/java/com/newsev/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("test conflict 2");
 		System.out.println("test conflict 1");
+		System.out.println("test with hot_fix");
 	}
 }
